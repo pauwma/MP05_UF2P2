bac1aa11ee8795dcda4820779d68ae8fc705ff12
pauwma <paumual2003@elpuig.xeill.net>
2023-02-15 19:26:45 +0100

    Initial commit

---
+++ src/test/java/HashTableTest.java
@@ -0,0 +1,38 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class HashTableTest {
+
+    @Test
+    void count() {
+    }
+
+    @Test
+    void size() {
+    }
+
+    @Test
+    void put() {
+    }
+
+    @Test
+    void get() {
+    }
+
+    @Test
+    void drop() {
+    }
+
+    @Test
+    void testToString() {
+    }
+
+    @Test
+    void getCollisionsForKey() {
+    }
+
+    @Test
+    void testGetCollisionsForKey() {
+    }
+}
